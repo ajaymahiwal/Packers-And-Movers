65d093f5bc08fd8ef48672ae57cf10cbcb682dfd
ajaymahiwal <ajaymahiwal5@gmail.com>
2023-09-29 17:28:27 +0530

    Files Updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const navbar = document.getElementsByClassName("navbar")[0];
 const menu = document.getElementsByClassName("menu")[0];
 
-menu.addEventListener("click",(e)=>{
-    console.log("clicked");
-    navbar.classList.toggle("navbar-menu");
-})
+// menu.addEventListener("click",(e)=>{
+//     console.log("clicked");
+//     navbar.classList.toggle("navbar-menu");
+// });
