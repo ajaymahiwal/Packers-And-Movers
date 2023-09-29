89111db81ed49ef48b116a43d94783a5bbe6fd04
ajaymahiwal <ajaymahiwal5@gmail.com>
2023-09-29 13:58:42 +0530

    Testing New Code Live

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const menu = document.getElementsByClassName("menu")[0];
 
 menu.addEventListener("click",(e)=>{
     console.log("clicked");
-    // navbar.classList.toggle("navbar-menu");
+    navbar.classList.toggle("navbar-menu");
 })
