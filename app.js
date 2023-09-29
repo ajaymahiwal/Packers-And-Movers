8747f502a9266a6da5ebf6adb8e2cef5e22bcc25
ajaymahiwal <ajaymahiwal5@gmail.com>
2023-09-29 10:15:53 +0530

    Code Updated

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const navbar = document.getElementsByClassName("navbar")[0];
+const menu = document.getElementsByClassName("menu")[0];
+
+menu.addEventListener("click",(e)=>{
+    console.log("clicked");
+    // navbar.classList.toggle("navbar-menu");
+})
